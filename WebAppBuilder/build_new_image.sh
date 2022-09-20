9e00182d61bba62c7116ce733f88aeded0eaadf7
Shoki Sugimoto <45707861+Loki1214@users.noreply.github.com>
2022-09-20 09:24:51 +0900

    a

--- WebAppBuilder/build_new_image.sh
+++ WebAppBuilder/build_new_image.sh
@@ -2,14 +2,14 @@
 
 LOG_OUT=log/stdout.log
 LOG_ERR=log/stderr.log
-# exec 1> >(
-# 	while read -r l; do echo "[$(date +"%Y-%m-%d %H:%M:%S")] $l"; done \
-# 		| tee -a $LOG_OUT
-# 	)
-# exec 2> >(
-# 	while read -r l; do echo "[$(date +"%Y-%m-%d %H:%M:%S")] $l"; done \
-# 	| tee -a $LOG_ERR 1>&2
-# 	)
+exec 1> >(
+	while read -r l; do echo "[$(date +"%Y-%m-%d %H:%M:%S")] $l"; done \
+		| tee -a $LOG_OUT
+	)
+exec 2> >(
+	while read -r l; do echo "[$(date +"%Y-%m-%d %H:%M:%S")] $l"; done \
+	| tee -a $LOG_ERR 1>&2
+	)
 
 baseDir=`dirname $0`
 modelDir=$1
@@ -25,7 +25,7 @@ cp --force ${modelDir}/* ${context}/app
 
 Registry=localhost:5050
 timestump=$(date "+%Y%m%d")
-docker build ${context} -t ${Registry}/mywebapp:${timestump} -t ${Registry}/mywebapp:latest
+docker build --no-cache ${context} -t ${Registry}/mywebapp:${timestump} -t ${Registry}/mywebapp:latest
 docker push -a ${Registry}/mywebapp
 build_result=$?
 
